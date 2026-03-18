e6abf213592a05acbefaed18a7846104becfa477
David Ebschke <davidebschke8@gmail.com>
2026-03-18 12:18:22 +0100

    delete unused import in OllamaService.java

--- my_ki_tool/backend/src/main/java/kitool/backend/service/OllamaService.java
+++ my_ki_tool/backend/src/main/java/kitool/backend/service/OllamaService.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Locale;
 
 public class OllamaService {
 
